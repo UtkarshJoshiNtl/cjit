8617279496d777e21a704012db185f9515b92e13
UtkarshJoshiNtl <joshiutkarshntl@gmail.com>
2026-04-10 22:58:53 +0530

    level-1: implement init command with .cjit directory creation

--- cjit.c
+++ cjit.c
@@ -2,10 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+void init_repo() {
+    system("mkdir .cjit");
+    FILE *f1 = fopen(".cjit/commits.txt", "w");
+    FILE *f2 = fopen(".cjit/staging.txt", "w");
+    fclose(f1);
+    fclose(f2);
+    printf("Initialized empty CJIT repository\n");
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Usage: cjit <command>\n");
         return 1;
+    }
+
+    if (strcmp(argv[1], "init") == 0) {
+        init_repo();
     }
 
     return 0;
