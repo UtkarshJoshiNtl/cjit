f50677419446107f2c99d79b76a2db288a51fea9
UtkarshJoshiNtl <joshiutkarshntl@gmail.com>
2026-04-10 22:40:38 +0530

    feat: add add command with staging logic

--- cjit.c
+++ cjit.c
@@ -11,6 +11,23 @@ void init_repo() {
     printf("Initialized empty CJIT repository\n");
 }
 
+void add_file(char *filename) {
+    char buffer[256];
+    FILE *f = fopen(filename, "r");
+    if (!f) {
+        printf("Error: cannot open %s\n", filename);
+        return;
+    }
+
+    while (fgets(buffer, 256, f) != NULL) {
+    }
+
+    FILE *staging = fopen(".cjit/staging.txt", "a");
+    fprintf(staging, "%s\n", filename);
+
+    printf("Staged: %s\n", filename);
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Usage: cjit <command>\n");
@@ -19,6 +36,12 @@ int main(int argc, char *argv[]) {
 
     if (strcmp(argv[1], "init") == 0) {
         init_repo();
+    } else if (strcmp(argv[1], "add") == 0) {
+        if (argc < 3) {
+            printf("Usage: cjit add <filename>\n");
+            return 1;
+        }
+        add_file(argv[2]);
     } else {
         printf("Unknown command: %s\n", argv[1]);
     }
