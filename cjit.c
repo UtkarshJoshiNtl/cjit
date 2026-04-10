0214688f1f39887cf20212ad60aab5418de4fc61
UtkarshJoshiNtl <joshiutkarshntl@gmail.com>
2026-04-10 22:40:07 +0530

    init: create basic file structure with init command

---
+++ cjit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void init_repo() {
+    system("mkdir .cjit");
+    FILE *f1 = fopen(".cjit/commits.txt", "w");
+    FILE *f2 = fopen(".cjit/staging.txt", "w");
+    fclose(f1);
+    fclose(f2);
+    printf("Initialized empty CJIT repository\n");
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: cjit <command>\n");
+        return 1;
+    }
+
+    if (strcmp(argv[1], "init") == 0) {
+        init_repo();
+    } else {
+        printf("Unknown command: %s\n", argv[1]);
+    }
+
+    return 0;
+}
