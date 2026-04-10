75cfd8ddf6ec83c895013dc1560a7ce87b44fa29
UtkarshJoshiNtl <joshiutkarshntl@gmail.com>
2026-04-10 23:13:13 +0530

    level-4: base with init, add, commit, checkout from level-3

--- cjit.c
+++ cjit.c
@@ -2,70 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Commit {
-    int id;
-    char message[100];
-};
-
-int next_commit_id = 1;
-
 void init_repo() {
     system("mkdir .cjit");
+    system("mkdir .cjit/objects");
     FILE *f1 = fopen(".cjit/commits.txt", "w");
     FILE *f2 = fopen(".cjit/staging.txt", "w");
     fclose(f1);
     fclose(f2);
     printf("Initialized empty CJIT repository\n");
-}
-
-void add_file(char *filename) {
-    char buffer[256];
-    FILE *f = fopen(filename, "r");
-    if (!f) {
-        printf("Error: cannot open %s\n", filename);
-        return;
-    }
-
-    while (fgets(buffer, 256, f) != NULL) {
-    }
-
-    FILE *staging = fopen(".cjit/staging.txt", "a");
-    fprintf(staging, "%s\n", filename);
-
-    printf("Staged: %s\n", filename);
-}
-
-void do_commit(char *message) {
-    struct Commit *c = malloc(sizeof(struct Commit));
-    c->id = next_commit_id++;
-    strcpy(c->message, message);
-
-    FILE *f = fopen(".cjit/commits.txt", "a");
-    fprintf(f, "%d|%s\n", c->id, c->message);
-
-    char staged_file[128];
-    FILE *staging = fopen(".cjit/staging.txt", "r");
-    FILE *commits_dir = fopen(".cjit/commits.txt", "r+");
-
-    while (fgets(staged_file, 128, staging)) {
-        staged_file[strlen(staged_file)-1] = '\0';
-        FILE *src = fopen(staged_file, "r");
-        if (src) {
-            char path[256];
-            sprintf(path, ".cjit/%d_%s", c->id, staged_file);
-            FILE *dest = fopen(path, "w");
-            char ch;
-            while ((ch = fgetc(src)) != EOF) {
-                fputc(ch, dest);
-            }
-        }
-    }
-
-    fclose(staging);
-    FILE *clear = fopen(".cjit/staging.txt", "w");
-    fclose(clear);
-
-    printf("Committed: %s (id: %d)\n", c->message, c->id);
 }
 
 int main(int argc, char *argv[]) {
@@ -81,15 +25,91 @@ int main(int argc, char *argv[]) {
             printf("Usage: cjit add <filename>\n");
             return 1;
         }
-        add_file(argv[2]);
+        FILE *f = fopen(argv[2], "r");
+        if (!f) {
+            printf("Error: cannot open %s\n", argv[2]);
+            return 1;
+        }
+        fclose(f);
+        FILE *staging = fopen(".cjit/staging.txt", "a");
+        fprintf(staging, "%s\n", argv[2]);
+        fclose(staging);
+        printf("Staged: %s\n", argv[2]);
     } else if (strcmp(argv[1], "commit") == 0) {
         if (argc < 3) {
             printf("Usage: cjit commit <message>\n");
             return 1;
         }
-        do_commit(argv[2]);
-    } else {
-        printf("Unknown command: %s\n", argv[1]);
+        static int commit_id = 1;
+        FILE *commits = fopen(".cjit/commits.txt", "a");
+        fprintf(commits, "%d|%s|", commit_id, argv[2]);
+
+        char staged_file[128];
+        FILE *staging = fopen(".cjit/staging.txt", "r");
+        while (fgets(staged_file, 128, staging)) {
+            staged_file[strlen(staged_file)-1] = '\0';
+            fprintf(commits, "%s,", staged_file);
+
+            char obj_path[256];
+            sprintf(obj_path, ".cjit/objects/%d_%s", commit_id, staged_file);
+            FILE *src = fopen(staged_file, "r");
+            FILE *dest = fopen(obj_path, "w");
+            char ch;
+            while ((ch = fgetc(src)) != EOF) {
+                fputc(ch, dest);
+            }
+            fclose(src);
+            fclose(dest);
+        }
+        fprintf(commits, "\n");
+        fclose(staging);
+        fclose(commits);
+
+        FILE *clear = fopen(".cjit/staging.txt", "w");
+        fclose(clear);
+        printf("Committed: %s (id: %d)\n", argv[2], commit_id);
+        commit_id++;
+    } else if (strcmp(argv[1], "checkout") == 0) {
+        if (argc < 3) {
+            printf("Usage: cjit checkout <commit_id>\n");
+            return 1;
+        }
+        int target_id = atoi(argv[2]);
+        FILE *commits = fopen(".cjit/commits.txt", "r");
+        char line[512];
+        int found = 0;
+        while (fgets(line, 512, commits)) {
+            int id;
+            sscanf(line, "%d|", &id);
+            if (id == target_id) {
+                found = 1;
+                char *files_start = strchr(line, '|');
+                files_start = strchr(files_start + 1, '|');
+                if (files_start) {
+                    files_start++;
+                    char *files = strtok(files_start, ",");
+                    while (files && strcmp(files, "\n") != 0) {
+                        char obj_path[256];
+                        sprintf(obj_path, ".cjit/objects/%d_%s", target_id, files);
+                        FILE *src = fopen(obj_path, "r");
+                        FILE *dest = fopen(files, "w");
+                        char ch;
+                        while ((ch = fgetc(src)) != EOF) {
+                            fputc(ch, dest);
+                        }
+                        fclose(src);
+                        fclose(dest);
+                        printf("Restored: %s\n", files);
+                        files = strtok(NULL, ",");
+                    }
+                }
+                break;
+            }
+        }
+        fclose(commits);
+        if (!found) {
+            printf("Commit %d not found\n", target_id);
+        }
     }
 
     return 0;
