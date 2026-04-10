c7a194201bd74fde6d15123fe5c84766a4c91e11
UtkarshJoshiNtl <joshiutkarshntl@gmail.com>
2026-04-10 22:41:27 +0530

    feat: add commit command with struct

--- cjit.c
+++ cjit.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct Commit {
+    int id;
+    char message[100];
+};
+
+int next_commit_id = 1;
+
 void init_repo() {
     system("mkdir .cjit");
     FILE *f1 = fopen(".cjit/commits.txt", "w");
@@ -28,6 +35,39 @@ void add_file(char *filename) {
     printf("Staged: %s\n", filename);
 }
 
+void do_commit(char *message) {
+    struct Commit *c = malloc(sizeof(struct Commit));
+    c->id = next_commit_id++;
+    strcpy(c->message, message);
+
+    FILE *f = fopen(".cjit/commits.txt", "a");
+    fprintf(f, "%d|%s\n", c->id, c->message);
+
+    char staged_file[128];
+    FILE *staging = fopen(".cjit/staging.txt", "r");
+    FILE *commits_dir = fopen(".cjit/commits.txt", "r+");
+
+    while (fgets(staged_file, 128, staging)) {
+        staged_file[strlen(staged_file)-1] = '\0';
+        FILE *src = fopen(staged_file, "r");
+        if (src) {
+            char path[256];
+            sprintf(path, ".cjit/%d_%s", c->id, staged_file);
+            FILE *dest = fopen(path, "w");
+            char ch;
+            while ((ch = fgetc(src)) != EOF) {
+                fputc(ch, dest);
+            }
+        }
+    }
+
+    fclose(staging);
+    FILE *clear = fopen(".cjit/staging.txt", "w");
+    fclose(clear);
+
+    printf("Committed: %s (id: %d)\n", c->message, c->id);
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Usage: cjit <command>\n");
@@ -42,6 +82,12 @@ int main(int argc, char *argv[]) {
             return 1;
         }
         add_file(argv[2]);
+    } else if (strcmp(argv[1], "commit") == 0) {
+        if (argc < 3) {
+            printf("Usage: cjit commit <message>\n");
+            return 1;
+        }
+        do_commit(argv[2]);
     } else {
         printf("Unknown command: %s\n", argv[1]);
     }
